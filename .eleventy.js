7f0d43e48ad6062b5f3aa3fc1c49f776af668c96
jamessvolos <svolos.james@outlook.com>
2026-06-30 12:34:18 -0700

    Remove dead Eleventy config

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false); // generated page templates are git-ignored but must still build
-  ["favicon.png", "CNAME", ".nojekyll", "robots.txt",
-   "llms.txt", "llms-full.txt", "assets", "about", "connect"]
-    .forEach((p) => eleventyConfig.addPassthroughCopy(p));
-  return { dir: { input: "src", output: "_site", includes: "_includes" } };
-};
